aefa1ce2e1d2cce339031edfc583630adc81bdae
moazzam023 <moazzam1311023@gmail.com>
2024-10-23 23:05:44 +0530

    build deployed

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-W4SKBIVU.mjs";import"./chunk-2KN4CZE5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-GN45L3AB.mjs";import"./chunk-2KN4CZE5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
